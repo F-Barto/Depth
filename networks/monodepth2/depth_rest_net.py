84b8417740926004642180460c52b1089f64e206
F-Barto <fbartoc@hotmail.fr>
2020-09-03 13:55:22 +0200

    add args to basic DepthResNet

--- networks/monodepth2/depth_rest_net.py
+++ networks/monodepth2/depth_rest_net.py
@@ -21,7 +21,8 @@ class DepthResNet(nn.Module):
     kwargs : dict
         Extra parameters
     """
-    def __init__(self, num_layers=18, activation='relu', preact=False, invertible=False, n_power_iterations=5, **kwargs):
+    def __init__(self, num_layers=18, input_channels=3, activation='relu', preact=False, invertible=False,
+                 n_power_iterations=5, no_maxpool=, **kwargs):
         super().__init__()
 
         assert num_layers in [18, 34, 50], 'ResNet version {} not available'.format(num_layers)
@@ -29,7 +30,8 @@ class DepthResNet(nn.Module):
         activation_cls = get_activation(activation)
 
         self.encoder = ResnetEncoder(num_layers=num_layers, activation=activation_cls, preact=preact,
-                                     invertible=invertible, n_power_iterations=n_power_iterations)
+                                     input_channels=input_channels, invertible=invertible, no_maxpool=no_maxpool,
+                                     n_power_iterations=n_power_iterations)
         self.decoder = DepthDecoder(num_ch_enc=self.encoder.num_ch_enc, activation=activation_cls, **kwargs)
         self.scale_inv_depth = partial(disp_to_depth, min_depth=0.1, max_depth=120.0)
 
