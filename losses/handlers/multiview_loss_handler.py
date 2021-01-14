59b987e5d65aeb21a67746c2b573059ee1213627
F-Barto <fbartoc@hotmail.fr>
2021-01-14 19:14:25 +0100

    debug

--- losses/handlers/multiview_loss_handler.py
+++ losses/handlers/multiview_loss_handler.py
@@ -11,7 +11,7 @@ from losses.loss_base import LossBase
 from losses.handlers.handler_base import LossHandler
 
 
-class MultiViewLossHandler(LossBase, LossHandler):
+class MultiViewLossHandler(LossHandler, LossBase):
     """
     Semi-Supervised loss for inverse depth maps.
 
